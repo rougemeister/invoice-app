3731ed81661e9e675d3f337810c1f59c3a5bae8f
rougemeister <rougemeister1@gmail.com>
2024-08-28 14:52:50 +0000

    Rendered items in invoice-list component

--- src/app/features/components/invoice/edit-invoice-form/edit-invoice-form.component.ts
+++ src/app/features/components/invoice/edit-invoice-form/edit-invoice-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../../../shared/components/nav-bar/nav-bar.component';
+import { ListItemComponent } from '../../../../shared/components/list-item/list-item.component';
 
 @Component({
   selector: 'app-edit-invoice-form',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent,ListItemComponent],
   templateUrl: './edit-invoice-form.component.html',
   styleUrl: './edit-invoice-form.component.scss'
 })
--- src/app/features/components/invoice/invoice-list/invoice-list.component.ts
+++ src/app/features/components/invoice/invoice-list/invoice-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { NavBarComponent } from '../../../../shared/components/nav-bar/nav-bar.component';
+import { ListItemComponent } from '../../../../shared/components/list-item/list-item.component';
+import { selectAllInvoices } from '../../store/selectors/invoice.selectors';
+import { loadInvoices } from '../../store/actions/invoice.actions';
+import { Observable } from 'rxjs';
+import { Invoice } from '../models/invoice.model';
+import { AppState } from '../../../../state/app.state';
+import { Store } from '@ngrx/store';
+
 
 @Component({
   selector: 'app-invoice-list',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent,ListItemComponent, AsyncPipe],
   templateUrl: './invoice-list.component.html',
   styleUrl: './invoice-list.component.scss'
 })
 export class InvoiceListComponent {
+  invoices$: Observable<Invoice[]>;  // Observable to hold the list of invoices
+
+  constructor(private store: Store<AppState>) {
+    this.invoices$ = this.store.select(selectAllInvoices);
+  }
+  
+  ngOnInit(): void {
+    this.store.dispatch(loadInvoices());
+  }
+
 
 }
+
+
