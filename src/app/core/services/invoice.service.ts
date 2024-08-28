d15824a701e69744a014f67d4a07d9a9ac76f4e1
rougemeister <rougemeister1@gmail.com>
2024-08-28 09:49:51 +0000

    Added core module file

---
+++ src/app/core/services/invoice.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Invoice } from '../../features/components/invoice/models/invoice.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class InvoiceService {
+  private jsonUrl = 'assets/invoices.json'; // Adjust this path as needed
+
+  constructor(private http: HttpClient) { }
+
+  getInvoices(): Observable<Invoice[]> {
+    return this.http.get<Invoice[]>(this.jsonUrl);
+  }
+
+  getInvoiceById(id: string): Observable<Invoice | undefined> {
+    return this.http.get<Invoice[]>(this.jsonUrl).pipe(
+      map(invoices => invoices.find(invoice => invoice.id === id))
+    );
+  }
+}
