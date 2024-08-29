189fa668bb23c834f23e979aae60e3ba356a41f3
rougemeister <rougemeister1@gmail.com>
2024-08-29 10:14:29 +0000

    Styled filter component

---
+++ src/app/shared/components/filter/filter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.scss'
+})
+export class FilterComponent {
+  
+
+}
