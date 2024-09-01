5f1df847486f33a6a5224bcbe6f6f02e0763ae9a
rougemeister <rougemeister1@gmail.com>
2024-09-01 07:11:32 +0000

    Added shadow and hover state to list item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InvoiceListComponent } from './features/components/invoice/invoice-list/invoice-list.component';
+import { InvoiceDetailsComponent } from "./features/components/invoice/invoice-details/invoice-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvoiceListComponent],
+  imports: [RouterOutlet, InvoiceListComponent, InvoiceDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/features/components/invoice/invoice-details/invoice-details.component.ts
+++ src/app/features/components/invoice/invoice-details/invoice-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../../../shared/components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-invoice-details',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, NavBarComponent],
   templateUrl: './invoice-details.component.html',
   styleUrl: './invoice-details.component.scss'
 })
