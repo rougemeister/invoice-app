0700281b03fb4c8de4743f0ac4df9f7a20bb0be6
rougemeister <rougemeister1@gmail.com>
2024-09-02 08:13:15 +0000

    .

--- src/app/features/components/invoice/new-invoice-form/new-invoice-form.component.ts
+++ src/app/features/components/invoice/new-invoice-form/new-invoice-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../../../shared/components/nav-bar/nav-bar.component';
+import { BackButtonComponent } from '../../../../shared/components/back-button/back-button.component';
 
 @Component({
   selector: 'app-new-invoice-form',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,BackButtonComponent],
   templateUrl: './new-invoice-form.component.html',
   styleUrl: './new-invoice-form.component.scss'
 })
